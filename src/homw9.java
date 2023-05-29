80ff82bb1eb3590fcd0ef1706d4d9e544c9f3639
Ruby K <rubykdec22@gmail.com>
2023-05-29 14:02:43 -0500

    committed

---
+++ src/homw9.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Random;
+
+public class homw9 {
+
+	
+		
+		   
+		
+		    public static void main(String args[])
+		    
+		    {
+		        // create instance of Random class
+		        Random rand = new Random();
+		   
+		        // Generate random integers in range 0 to 500
+		        int rand_int1 = rand.nextInt(500);
+		        int rand_int2 = rand.nextInt(500);
+		   
+		        // Print random integers
+		        System.out.println("Random Integers: "+rand_int1);
+		        System.out.println("Random Integers: "+rand_int2);
+		   
+		   
+		   
+		    }
+	}
+
+
